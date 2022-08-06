b29db01029729bc4cae5433783fefb1d61a12881
siakira <106393048+siakira@users.noreply.github.com>
2022-08-07 03:58:33 +0900

    Update il2cpp_dump.cpp

--- module/src/main/cpp/il2cpp_dump.cpp
+++ module/src/main/cpp/il2cpp_dump.cpp
@@ -350,8 +350,8 @@ void il2cpp_dump(void *handle, char *outDir) {
     LOGI("il2cpp_handle: %p", handle);
     il2cpp_handle = handle;
     auto outPath1 = std::string(outDir).append("/files/dump.so");
-    FILE *fp = fopen(outPath1, "w");
-    fclose(fp);
+    //FILE *fp = fopen(outPath1, "w");
+    //fclose(fp);
     init_il2cpp_api();
     if (il2cpp_domain_get_assemblies) {
         Dl_info dlInfo;
