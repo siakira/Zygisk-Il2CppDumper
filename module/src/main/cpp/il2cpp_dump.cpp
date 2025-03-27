ed89583af58622bd534d6389e080840b0b543f1a
siakira <106393048+siakira@users.noreply.github.com>
2025-03-28 05:52:37 +0900

    Update il2cpp_dump.cpp

--- module/src/main/cpp/il2cpp_dump.cpp
+++ module/src/main/cpp/il2cpp_dump.cpp
@@ -439,8 +439,11 @@ void il2cpp_dump(const char *outDir) {
     
     }
    const char* byte_stream = static_cast<const char*>(page_start);
-    auto path_adr = std::format("/files/dumpcpp0x%"PRIX64".so", il2cpp_base);
-    auto dump_outPath = std::string(outDir) + path_adr;
+    std::stringstream path_str;
+    path_str << "/files/dumpcpp";
+    path_str << std::hex << il2cpp_base;
+    path_str << ".so";   
+    auto dump_outPath = std::string(outDir) + path_str.str();
     std::ofstream ofs(dump_outPath, std::ios::binary | std::ios::trunc);
     ofs.write(byte_stream, static_cast<std::streamsize>(hack_size));
   
