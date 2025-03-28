bdbbed8c7014da81f0519279af48c3b35d56fda9
siakira <106393048+siakira@users.noreply.github.com>
2025-03-28 20:58:47 +0900

    Update il2cpp_dump.cpp

--- module/src/main/cpp/il2cpp_dump.cpp
+++ module/src/main/cpp/il2cpp_dump.cpp
@@ -467,7 +467,7 @@ void il2cpp_dump(const char *outDir) {
     LOGI("method %p", base_addr);
     unsigned long hack_addr = reinterpret_cast<unsigned long>(base_addr) + 0x3C;//偏移;
     */
-    unsigned long hack_addr = static_cast<unsigned long>(il2cpp_base) +0x3CCC370;
+    unsigned long hack_addr = static_cast<unsigned long>(il2cpp_base) +0x3CCC4D8;
     //设置属性可写
     
      void* page_start = (void*)(hack_addr - hack_addr % PAGE_SIZE);
@@ -477,10 +477,10 @@ void il2cpp_dump(const char *outDir) {
     }
  
     unsigned char* tmp = (unsigned char*)(void*)hack_addr;
-    tmp[0] = 0x39;
-    tmp[1] = 0x46;
-    tmp[2] = 0x00;
-    tmp[3] = 0x14;
+    tmp[0] = 0xC0;
+    tmp[1] = 0x03;
+    tmp[2] = 0x5F;
+    tmp[3] = 0xD6;
 
     LOGI("hack End");
     
